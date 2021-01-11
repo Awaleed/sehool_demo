9bb1b284c12858b4088e8dbdb7032f8a08cac4ec
||kubur|| <31385163+mamoun-kubur@users.noreply.github.com>
2021-01-11 16:44:43 +0200

    screens

---
+++ lib/screens/login.dart
@@ -0,0 +1,98 @@
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+import 'package:sehool/screens/home.dart';
+import 'package:sehool/screens/signup.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset('assets/logo.png'),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: new InputDecoration(
+                    suffixIcon: Icon(Icons.person),
+                    border: new OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(20.0),
+                      ),
+                    ),
+                    filled: true,
+                    hintStyle: new TextStyle(color: Colors.grey[400]),
+                    hintText: 'username',
+                    fillColor: Colors.white70),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: new InputDecoration(
+                    suffixIcon: Icon(Icons.lock),
+                    border: new OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(20.0),
+                      ),
+                    ),
+                    filled: true,
+                    hintStyle: new TextStyle(color: Colors.grey[400]),
+                    hintText: 'password',
+                    fillColor: Colors.white70),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              RaisedButton(
+                  child: Txt(
+                    'login',
+                    style: TxtStyle()..textColor(Colors.white),
+                  ),
+                  color: Colors.blue,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Home()),
+                    );
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: BorderSide(color: Colors.blue))),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Txt('if not registerd '),
+                  GestureDetector(
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Signup()),
+                        );
+                      },
+                      child: Txt(
+                        'create acount?',
+                        style: TxtStyle()..textColor(Colors.blue),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/tabs/profiletab.dart
@@ -0,0 +1,45 @@
+import 'dart:developer';
+
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+
+class ProfileTab extends StatelessWidget {
+  const ProfileTab({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Parent(
+              style: ParentStyle()
+                ..border(all: 1, color: Colors.grey)
+                ..borderRadius(all: 10),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 40,
+                              backgroundColor: Colors.grey,
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
